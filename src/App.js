7ebfad537cd150b91a616142b7f4a2dc2d461f5e
Yong Liang <yongl9460@gmail.com>
2019-03-20 17:30:23 +1100

    Added style-component to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Tab, Tabs, TabList, TabPanel, TabStyle } from 'react-tabs';
+// import "../node_modules/react-tabs/style/react-tabs.css"
+import styled from 'styled-components'
+
+const Heading = styled(Tab)`
+  color: red;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Tabs>
+          <TabList>
+            <Heading>Title 1</Heading>
+            <Heading>Title 2</Heading>
+          </TabList>
+      
+          <TabPanel>
+            <h2>Any content 1</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
     );
   }
 }
